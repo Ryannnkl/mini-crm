56fc3fde0ba580b702eb6853b13d435de74289f4
Ryann Ferreira <ryannnkl@gmail.com>
2025-11-10 22:25:53 -0300

    feat: add expo plugin

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,11 @@
 import { betterAuth } from "better-auth";
+import { expo } from "@better-auth/expo";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db";
 import * as schema from "@/db/schema";
 
 export const auth = betterAuth({
+  plugins: [expo()],
   database: drizzleAdapter(db, {
     provider: "pg",
     schema,
